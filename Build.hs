0a957b3d49b3300f06dd2d1a29cb659e49132f6b
Liam Naddell <liamnprg@gmail.com>
2018-02-19 10:35:10 -0500

    shake build

---
+++ Build.hs
@@ -0,0 +1,25 @@
+import Development.Shake
+import Development.Shake.Command
+import Development.Shake.FilePath
+import Development.Shake.Util
+
+main :: IO ()
+main = shakeArgs shakeOptions{shakeFiles="_build"} $ do
+    want ["_build/collatzGraph" <.> exe]
+
+    phony "clean" $ do
+        putNormal "Cleaning files in _build"
+        removeFilesAfter "_build" ["//*"]
+        cmd "rm -f Main.hi Lib/Lib.hi"
+
+    "_build/collatzGraph" <.> exe %> \out -> do
+        need ["_build/Lib.o","_build/Main.o"]
+        cmd "ghc _build/Lib.o _build/Main.o -o _build/collatzGraph"
+
+    "_build/Lib.o" %> \out -> do
+        need ["Lib/Lib.hs"]
+        cmd "ghc -c Lib/Lib.hs -o _build/Lib.o"
+
+    "_build/Main.o" %> \out -> do
+        need ["_build/Lib.o"]
+        cmd "ghc _build/Lib.o Main.hs -c -o _build/Main.o"
--- Makefile
+++
@@ -1,5 +0,0 @@
-all:
-	ghc --make main.hs -o collatzGraph
-
-clean:
-	rm -f collatzGraph main.hi main.o Lib.hi Lib/Lib.hi Lib/Lib.o
